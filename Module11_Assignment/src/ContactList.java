80695a04eca9b9c4031a0bc2baa9a6a3b961f838
dargos82 <142857091+dargos82@users.noreply.github.com>
2023-11-09 22:22:08 -0500

    Having an issue passing the file name variable "fileName" to the display() method (line 91)

--- Module11_Assignment/src/ContactList.java
+++ Module11_Assignment/src/ContactList.java
@@ -1,12 +1,99 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+
+/** class ContactList opens a file, and can add/delete data to that file, and print
+ * the file. 
+ * 
+ * @authors George Haralampopulos and David Blossom
+ */
 public class ContactList 
 {
-
-   public static void main(String[] args) 
+   public enum options
    {
-
+      ADD,
+      DELETE,
+      DISPLAY
+   }
+   
+   public String intro()
+   {
+      Scanner input = new Scanner(System.in);
+      System.out.println( "Welcome to the contact list.");
+      System.out.print( "File path and name: "); //get user input for file path and name
+      String fileName = input.next();
       
+      return fileName;
+   } //end intro()
+   
+   public int userSelection()
+   {
+      Scanner input = new Scanner(System.in);
+      
+      System.out.println( "\n1: Add contact" );
+      System.out.println( "2: Delete contact" );
+      System.out.println( "3: Display contact list" );
+      System.out.print( "Selection: ");
+      int selection = input.nextInt();
+      
+      return selection;
+   } //end userSelection()
+   
+   public void add()
+   {
+      
+   }
+   
+   public void delete()
+   {
+      
+   }
+   
+   public void display(String fileName) throws IOException
+   {
+      BufferedReader displayList = null;
+      String record = null;
+      
+      try
+      {
+         displayList = new BufferedReader( new FileReader( fileName ) );
+         
+         while( ( record = displayList.readLine() ) !=  null)
+            System.out.println( record ); //print data from file
+         
+      } catch (FileNotFoundException e)
+      {
+         e.printStackTrace();
+      }
+   }
+   
+
+   public static void main(String[] args) throws IOException 
+   {
+      ContactList list = new ContactList();
+      
+      File file = new File( list.intro() ); //create new File object
+      
+      switch( list.userSelection() )
+      {
+         case 1:
+            list.add();
+         case 2:
+            list.delete();
+         case 3:
+            list.display( list.intro().toString() );
+         default:
+            System.out.println( "Invalid response.");
+      }      
+      
+      //fileOutput.close();
       
    } //end main()
-
 } //end ContactList
